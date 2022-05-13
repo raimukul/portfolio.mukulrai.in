271c59e416c2d226d980d69505408f782491b46a
btrchain <47410631+raimukul@users.noreply.github.com>
2022-05-13 22:36:23 +0530

    file removal

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -7,7 +7,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://old.mukulrai.in/ResumeMukulRai.pdf";
+  "./files/ResumeMukulRai.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
